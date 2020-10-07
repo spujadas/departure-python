2cca25f1d6e7e491c84129c5c75f259eac45cdb6
sebp <spujadas@gmail.com>
2020-10-07 22:25:06 +0200

    extract logging to commons

--- departure/commons.py
+++ departure/commons.py
@@ -13,3 +13,17 @@ def log_function_stdout_to_debug(
     with redirect_stdout(stdout_string):
         function(*argv)
     logger.debug(stdout_string.getvalue())
+
+def init_logging(logger_level=logging.DEBUG, handler_level=logging.DEBUG):
+    logger = logging.getLogger('departure')
+    logger.setLevel(level=logger_level)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(handler_level)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+    return logger
