ca7439b8742a81c194aa11b8458cd6968e47bf3a
sebp <spujadas@gmail.com>
2020-10-10 23:02:42 +0200

    update setup.py for packaging

--- setup.py
+++ setup.py
@@ -4,9 +4,19 @@ Setup for departure module
 
 from setuptools import setup, find_namespace_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="departure",
-    version="1.0",
+    version="1.0.0",
+    author="Sébastien Pujadas",
+    author_email="sebastien@pujadas.net",
+    description="Get station information and departures from public transport "
+        "operators, and control virtual or physical departure boards",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spujadas/departure-python",
     packages=find_namespace_packages(include=["departure.*"]),
     entry_points={
         "console_scripts": [
@@ -28,4 +38,10 @@ setup(
         "tabulate",
     ],
     include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
