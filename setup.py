c4a9503fae15cc5baed4850009c5a11c77e1eacd
sebp <spujadas@gmail.com>
2020-10-10 16:29:32 +0200

    rename main CLI script

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="departure",
     version="1.0",
-    packages=find_namespace_packages(include=['departure.*']),
+    packages=find_namespace_packages(include=["departure.*"]),
     entry_points={
         "console_scripts": [
-            "departure-client=departure.cli.client:entry_point",
+            "departure=departure.cli.client:entry_point",
             "departure-server=departure.cli.server:entry_point",
-            "departure-web=departure.cli.web:start"
+            "departure-web=departure.cli.web:start",
         ],
     },
     install_requires=[
@@ -27,5 +27,5 @@ setup(
         "fastapi",
         "tabulate",
     ],
-    include_package_data=True
+    include_package_data=True,
 )
